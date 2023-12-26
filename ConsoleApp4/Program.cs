dd6e48f413cead3868693a72945d7f1c7b7ba22d
fidannh <fidannh@code.edu.az>
2023-12-26 14:40:04 +0400

    Completed

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,98 @@
+﻿#region  Task
+//Console.Write("Yazi daxil edin: ");
+//        string text = Console.ReadLine();
+//        int endIndex = text.Length - 1;
+//        string newText = "";
+//        while (endIndex >= 0 && text[endIndex] == ' ')
+//        {
+//            endIndex--;
+//        }
+//        for (int i = 0; i <= endIndex; i++)
+//        {
+//           newText += text[i];
+//        }
+
+//        Console.WriteLine("Sonundaki boshluglar silinmish yazi:"+newText);
+#endregion
+
+#region CalcApp
+//bool check = true;
+//double firstNum;
+//double secondNum;
+
+//do
+//{
+
+//    Console.WriteLine("1)Toplama");
+//    Console.WriteLine("2)Chixma");
+//    Console.WriteLine("3)Hasil");
+//    Console.WriteLine("4)Nisbet");
+//    Console.WriteLine("0)Programdan chixish");
+
+//    string  option=Console.ReadLine();
+
+//    switch(option)
+//    {
+//        case "1":
+//            Console.WriteLine("Toplama");
+//            Console.WriteLine("1ci ededi daxil edin:");
+//            firstNum=Convert.ToDouble(Console.ReadLine());
+//            Console.WriteLine("2ci ededi daxil edin:");
+//            secondNum = Convert.ToDouble(Console.ReadLine());
+//            Console.WriteLine($"Netice: {firstNum+secondNum}");
+//            break;
+
+
+
+//        case "2":
+//            Console.WriteLine("Chixma");
+//            Console.WriteLine("1ci ededi daxil edin:");
+//            firstNum = Convert.ToDouble(Console.ReadLine());
+//            Console.WriteLine("2ci ededi daxil edin:");
+//            secondNum = Convert.ToDouble(Console.ReadLine());
+//            Console.WriteLine($"Netice: {firstNum - secondNum}");
+//            break;
+
+
+
+//            case "3":
+//            Console.WriteLine("Hasil");
+//            Console.WriteLine("1ci ededi daxil edin:");
+//            firstNum = Convert.ToDouble(Console.ReadLine());
+//            Console.WriteLine("2ci ededi daxil edin:");
+//            secondNum = Convert.ToDouble(Console.ReadLine());
+//            Console.WriteLine($"Netice: {firstNum * secondNum}");
+//            break;
+
+
+
+//            case "4":
+//            Console.WriteLine("Nisbet");
+//            Console.WriteLine("1ci ededi daxil edin:");
+//            firstNum = Convert.ToDouble(Console.ReadLine());
+//            Console.WriteLine("2ci ededi daxil edin:");
+//            secondNum = Convert.ToDouble(Console.ReadLine());
+
+//            if (secondNum != 0)
+//            {
+//                Console.WriteLine($"Netice: {firstNum/secondNum}");
+//            }
+
+//            else
+//            {
+//                check = false;
+//                Console.WriteLine("Xeta: 0-a bolmek mumkun deyil!");
+//            }
+//            break;
+
+//            case "0":
+//            Console.WriteLine("Program yekunlashdi...");
+//            check= false; break;
+ 
+//            default:
+//            Console.WriteLine("Yalnish emeliyyat!!");
+//            break;
+
+//    }
+//} while (check);
+#endregion
